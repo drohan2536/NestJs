e4f39d829faad24d6dfd06039240c342bb0dff94
drohan2536 <rohandhonde36@gmail.com>
2024-07-02 15:07:00 +0530

    route parameters handling

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,17 +1,31 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
-import {Request, Response} from 'express';
+import { Body, Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { createUserDto } from 'src/users/dtos/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
     @Get()
-    getUsers(){
-        return [{username: 'rd', email: 'rd1234@gmail.com'}];
+    getUsers() {
+        return [{ username: 'rd', email: 'rd1234@gmail.com' }];
     }
 
     @Post()
-    createUser(@Body() userData: createUserDto ){
+    createUser(@Body() userData: createUserDto) {
         console.log(userData);
-        return {} ;
+        return {};
+    }
+
+    // //Route parameters by express way
+    @Get(':id')
+    getUserId(@Req() request: Request, @Res() response: Response) {
+        console.log(request.params);
+        response.send(request.params);
+    }
+
+    // Route parameters by nest way(we can use nested param)
+    @Get(':id/:postId')
+    getUserByID(@Param('id') id: string, @Param('postId') postID: string ){
+        console.log(id,postID);
+        return{id, postID};
     }
 }
