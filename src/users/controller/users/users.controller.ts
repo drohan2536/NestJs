2f442fde9f848f007221728d41ed73992076bc1f
drohan2536 <rohandhonde36@gmail.com>
2024-07-02 14:30:13 +0530

    handing post request

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {Request, Response} from 'express';
 @Controller('users')
 export class UsersController {
     @Get()
@@ -42,5 +42,12 @@ export class UsersController {
                 ]
             }
         ]
+    }
+
+    @Post()
+    createUser(@Req() request: Request, @Res() response: Response){
+        console.log(request.body);
+        response.send('Created');
+
     }
 }
